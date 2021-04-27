f2711921be0779749f45827d80e3e8a6b59e98ca
Killer-programmer <65546124+Killer-programmer@users.noreply.github.com>
2021-04-27 14:50:28 +0530

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "If 5x+32 = 4-2x then what is the value of x ?",
+    answer: "-4",
+    options: [
+      "-3",
+      "-4",
+      "7",
+      "4"
+    ]
+  },
+    {
+    numb: 2,
+    question: "The hottest planet in the solar system?",
+    answer: "Venus",
+    options: [
+      "Mercury",
+      "Venus",
+      "Jupiter",
+      "Mars"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Where was the electricity supply first introduced in India ?",
+    answer: "Darjeeling",
+    options: [
+      "Mumbai",
+      "Dehradun",
+      "Darjeeling",
+      " Chennai"
+    ]
+  },
+    {
+    numb: 4,
+    question: "First human heart transplant operation conducted by Dr. Christian Bernard, was conducted in?",
+    answer: "1967",
+    options: [
+      "1968",
+      "1967",
+      "1922",
+      "1958"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What is the full form of KGD?",
+    answer: "Krishna Gopal Dhal",
+    options: [
+      "Krishna Gokul Dhal",
+      "Krishna Gopal Dhar",
+      "Krishna Gopal Dhal",
+      "Krishna Golok Dhal"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
